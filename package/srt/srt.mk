feafe0ec29a80036a4937406df14929e09c6e311
Mike Goins <hammer@ballpeensoftware.com>
2025-08-28 15:47:50 -0400

    Rev SRT to 1.5.4 and enable bonding.

--- package/srt/srt.mk
+++ package/srt/srt.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-# this id is for release 1.4.4
-SRT_VERSION = 8b32f3734ff6af7cc7b0fef272591cb80a2d1aae
+SRT_VERSION = v1.5.4
 SRT_SITE = https://github.com/Haivision/srt/tarball/$(SRT_VERSION)
 SRT_INSTALL_STAGING = YES
 SRT_LICENSE = MPLv2.0
@@ -13,7 +12,7 @@ SRT_LICENSE_FILES = LICENSE
 
 SRT_DEPENDENCIES = host-cmake gnutls
 
-SRT_CONF_OPTS += -DUSE_ENCLIB=gnutls
+SRT_CONF_OPTS += -DUSE_ENCLIB=gnutls -DENABLE_BONDING=ON
 
 $(eval $(cmake-package))
 
